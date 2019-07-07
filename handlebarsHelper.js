e18267e7c89de435bc7d3cd9e815b27c5898136e
JudyLin <www.jiuky@gmail.com>
2019-07-07 17:47:47 +0800

    modify index css

--- handlebarsHelper.js
+++ handlebarsHelper.js
@@ -1,20 +1,37 @@
 const Handlebars = require('handlebars')
 
-Handlebars.registerHelper('if', function (context, options) {
+Handlebars.registerHelper('ifcategoryicon', function (context, options) {
 	console.log('this.category',this.category)
 	if(this.category === 'homeProperty'){
-    return options.fn({ image: 'home'})
+    return options.fn({ categoryimage: 'home'})
 	}
 	if(this.category === 'traffic'){
-    return options.fn({ image: 'shuttle-van'})
+    return options.fn({ categoryimage: 'shuttle-van'})
 	}
 	if(this.category === 'entertainment'){
-    return options.fn({ image: 'grin-beam'})
+    return options.fn({ categoryimage: 'grin-beam'})
 	}
 	if(this.category === 'food'){
-    return options.fn({ image: 'utensils'})
+    return options.fn({ categoryimage: 'utensils'})
 	}
   if(this.category === 'others'){
-    return options.fn({ image: 'pen'})
+    return options.fn({ categoryimage: 'pen'})
 	}
+})
+
+Handlebars.registerHelper('ifdate', function (context, options) {
+  let month = [
+    "Jan", "Feb", "Mar",
+    "Apr", "May", "Jun", "Jul",
+    "Aug", "Sep", "Octr",
+    "Nov", "Dec"
+  ];
+
+  let day = this.date.getDate();
+  let monthIndex = this.date.getMonth();
+  let year = this.date.getFullYear();
+	deteContent = day + '/' + month[monthIndex] + '/' + year
+	console.log(day + '/' + month[monthIndex] + '/' + year) 
+	
+  return options.fn({date: deteContent})
 })
