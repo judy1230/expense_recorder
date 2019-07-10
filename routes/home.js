6785be01a66625ae4d1642e9ff95b09e9644329c
JudyLin <www.jiuky@gmail.com>
2019-07-10 17:01:21 +0800

    draft chart display

--- routes/home.js
+++ routes/home.js
@@ -4,103 +4,36 @@ const router = express.Router()
 const Record= require('../models/record.js')
 const { authenticated } = require('../config/auth.js')
 require('../handlebarsHelper')
-const { CanvasRenderService } = require('chartjs-node-canvas');
 let totalAmount = 0
-Handlebars = require('handlebars')
-const ChartJs = require('chart.js');
-const { createCanvas } = require('canvas');
-var labels = ['高興', '難過'];
-const configuration = {
-	type: 'pie',
-	data: {
-		labels: labels,
-		datasets: [{
-			//預設資料
-			data: [10, 90],
-			backgroundColor: [
-				//資料顏色
-				"#00A1FF",
-				"#FF0004"
-			],
-		}],
-	}
-};
 
-const chartCallback = (ChartJS) => {
 
-	// Global config example: https://www.chartjs.org/docs/latest/configuration/
-	ChartJS.defaults.global.elements.rectangle.borderWidth = 2;
-	// Global plugin example: https://www.chartjs.org/docs/latest/developers/plugins.html
-	ChartJS.plugins.register({
-		// plugin implementation
-	});
-	// New chart type example: https://www.chartjs.org/docs/latest/developers/charts.html
-	ChartJS.controllers.MyType = ChartJS.DatasetController.extend({
-		
-	});
-};
-function renderPNG(configuration) {
-
-	const canvas = createCanvas(200, 200);
-	// Disable animation (otherwise charts will throw exceptions)
-	configuration.options = configuration.options || {};
-	configuration.options.responsive = false;
-	configuration.options.animation = false;
-	canvas.style = {};
-
-	const context = canvas.getContext('2d');
-	const canvasRenderService = new CanvasRenderService(200, 200);
-	const stream = canvasRenderService.renderToStream(configuration);
-	//chart = new ChartJs(context, configuration)
-	console.log('image ', image )
-	
-	return new Promise((resolve, reject) => {
-		// or `pngStream` `toDataURL`, etc
-		stream.canvas.rendertoBuffer((error, buffer) => {
-			if (error) {
-				return reject(error);
-			}
-			return resolve(buffer);
-		}).catch (error) 
-	});
-}
-router.get('/chart', async (req, res, next) => {
-	try {
-		const canvasRenderService = new CanvasRenderService(200,200);
-		const image = await canvasRenderService.renderToBuffer(configuration);
-		console.log('image' , image)
-		res.type('image/png');
-		res.send(image);
-	} catch (error) {
-		next(error);
-	}
-});
-
-//console.log('renderPNG(configuration)', renderPNG(configuration))
 //record首頁
 router.get('/', authenticated, (req, res) =>{
 	
-	req.flash('success_msg','welcome! 你已成功登入')
 	
-	const canvasRenderService = new CanvasRenderService(200, 200);
-	const stream = canvasRenderService.renderToDataURL(configuration);
-	//renderPNG(configuration)
-	Record.find({ userID: req.user._id })
-	.sort({date : 1})
-	.exec((err, records) =>{
+	req.flash('success_msg','welcome! 你已成功登入')
+	filteredMonth = 7
+	Record.find({ userID: req.user._id }, (err, records) =>{
+		const recordNow = records.filter(({ date}) => {
+			return date.getMonth(filteredMonth)	
+		})
+	})
+	  .sort({date : 1})
+		.exec((err, recordNow) =>{
 		//console.log('records',records)
 		if(err) return console.error(err)
 		if (totalAmount == 0){
-      for (let i = 0; i < records.length; i++){		
-			totalAmount += parseInt(records[i].amount)
+			for (let i = 0; i < recordNow.length; i++){		
+				totalAmount += parseInt(recordNow[i].amount)
 		  }
 		}
-	
+		let chartData = [10,20,40,15,15]
     //res.type('image/png')
 	  res.render('index', { 
-			records : records, 
+			records: recordNow, 
 			totalAmount : totalAmount,
-			myChart: stream
+			filteredMonth :  filteredMonth,
+			chartData : chartData
 		})
 	})
 })
@@ -135,7 +68,8 @@ router.get('/filter', authenticated, (req, res) => {
 		return res.render('index', { 
 			records: recordSearch, 
 			totalAmount: totalAmount, 
-			filteredCategory : filteredCategory, filteredMonth : filteredMonth  })
+			filteredCategory : filteredCategory,
+			filteredMonth : filteredMonth  })
   })
 })
 
