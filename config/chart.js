43d346e52af9ce492d227ddd5ff97772858d3554
JudyLin <www.jiuky@gmail.com>
2019-09-25 15:01:20 +0800

    finish /home, /filter chart display and total amount refactoring

--- config/chart.js
+++ config/chart.js
@@ -3,110 +3,78 @@ const Record = require('../models/record.js')
 module.exports = {
 	
 	getChart: async(req, res) => {
+		let totalAmount = 0
+		let itemsPerValue = 0
+		const filteredMonth = parseInt(req.query.month)
+		const filteredCategory = req.query.category
 		
 		try{
+			
 
-			let totalAmount = 0
-			let valueOfHomeProperty = 0
-			let valueOfTraffic = 0
-			let valueOfEntertainment = 0
-			let valueOfFood = 0
-			let valueOfOthers = 0
-			const filteredMonth = parseInt(req.query.month)
-			const filteredCategory = req.query.category
-			console.log('filteredMonth in chartjs', filteredMonth)
-			const recordsDisplay = await Record.find({
-				userID: req.user._id, date: {
+			if (filteredCategory !== 'all'){
+				const recordsDisplay = await Record.find({
+					userID: req.user._id, date: {
 					$gte: `2019-0${filteredMonth}-01T12:11:28.774+00:00`,
-					$lt: `2019-0${filteredMonth + 1}-01T12:11:28.774+00:00`
-				}
-			})
-			//console.log('recordsDisplay in chart', recordsDisplay  )
-			const recordSearch = recordsDisplay.filter(({ category }) => {
-				if (filteredCategory === 'all' ) {
-					return recordsDisplay
-				}
-				if (filteredCategory !== 'all') {
-					return category.includes(filteredCategory)
-				}
-			})
-			//console.log('recordSearch in chartjs', recordSearch)
-			const chartDataHomeProperty = await recordSearch.filter(({ date, category }) => {
-				return category.includes('homeProperty')
-			})
-
-			const chartDataTraffic = await recordSearch.filter(({ date, category }) => {
-				return category.includes('traffic')
-			})
-
-			const chartDateEntertainment = await recordSearch.filter(({ date, category }) => {
-				return category.includes('entertainment')
-			})
-
-			const chartDataFood = recordSearch.filter(({ date, category }) => {
-				return category.includes('food')
-			})
-
-			const chartDataOthers = await recordSearch.filter(({ date, category }) => {
-				return category.includes('others')
-			})
-			if (totalAmount == 0) {
-				for (let i = 0; i < recordSearch.length; i++) {
-					totalAmount += parseInt(recordSearch[i].amount)
-				}
-				for (let i = 0; i < chartDataHomeProperty.length; i++) {
-					valueOfHomeProperty += (chartDataHomeProperty[i].amount)
-					valueOfHomeProperty = Math.round(valueOfHomeProperty) / totalAmount * 100
-					if (valueOfHomeProperty > 100) {
-						valueOfHomeProperty = 100
-					}
-				}
-				for (let i = 0; i < chartDataTraffic.length; i++) {
-					valueOfTraffic += (chartDataTraffic[i].amount) / totalAmount * 100
-					valueOfTraffic = Math.round(valueOfTraffic)
-					if (valueOfTraffic > 100) {
-						valueOfTraffic = 100
-					}
-				}
-				for (let i = 0; i < chartDateEntertainment.length; i++) {
-					valueOfEntertainment += (chartDateEntertainment[i].amount) / totalAmount * 100
-					valueOfEntertainment = Math.round(valueOfEntertainment)
-					if (valueOfEntertainment > 100) {
-						valueOfEntertainment = 100
-					}
-				}
-				for (let i = 0; i < chartDataFood.length; i++) {
-					valueOfFood += (chartDataFood[i].amount) / totalAmount * 100
-					valueOfFood = Math.round(valueOfFood)
-					if (valueOfFood > 100) {
-						valueOfFood = 100
-					}
-				}
-				for (let i = 0; i < chartDataOthers.length; i++) {
-					valueOfOthers += (chartDataOthers[i].amount) / totalAmount * 100
-					valueOfOthers = Math.round(valueOfOthers)
-					if (valueOfOthers > 100) {
-						valueOfOthers = 100
-					}
-				}
-
-				let chartData = [valueOfHomeProperty, valueOfTraffic, valueOfEntertainment, valueOfFood, valueOfOthers]
-				console.log('chartData', chartData)
+					$lt: `2019-0${filteredMonth + 1}-01T12:11:28.774+00:00`},
+					category: filteredCategory
+				})
+				totalAmount = Amount(recordsDisplay)
+				chartData = getCategoryItem(recordsDisplay)
 				res.render('index', {
-					records: recordSearch,
+					records: recordsDisplay,
+					totalAmount: totalAmount,
+					filteredCategory: filteredCategory,
+					filteredMonth: filteredMonth,
+					chartData: chartData
+				})	
+			} else {
+				const recordsDisplay = await Record.find({
+					userID: req.user._id, date: {
+						$gte: `2019-0${filteredMonth}-01T12:11:28.774+00:00`,
+						$lt: `2019-0${filteredMonth + 1}-01T12:11:28.774+00:00`}
+				})
+				totalAmount = Amount(recordsDisplay)
+				chartData = getCategoryItem(recordsDisplay)
+				res.render('index', {
+					records: recordsDisplay,
 					totalAmount: totalAmount,
 					filteredCategory: filteredCategory,
 					filteredMonth: filteredMonth,
 					chartData: chartData
 				})
-			} 
-    		
+			}
+
+			function Amount(array) {
+				array.forEach((item) =>
+					totalAmount += parseInt(item.amount)
+				)
+				return totalAmount
+			}
+
+			function getCategoryItem(data) {
+				let chartData = []
+				let categoryItemArray = ['homeProperty', 'traffic', 'entertainment', 'food', 'others']
+				categoryItemArray.forEach(function (items) {
+					const chartDataPerItem = data.filter(({ category }) => {
+						return category.includes(items)
+					})
+					if (Array.isArray(chartDataPerItem) && chartDataPerItem.length) {
+						chartDataPerItem.forEach((item) => {
+							itemsPerValue += parseInt(item.amount)
+							itemsPerValue = Math.round(itemsPerValue / totalAmount * 100)
+						})
+					} else {
+						itemsPerValue = 0
+					}
+					chartData.push(itemsPerValue)
+				})
+				return chartData
+			}
+			
 		} catch (err) {
 			console.log(err)
-		}
-	//	return next()
-	}		
-		
-	}
+		}	
+	}				
+}
 
 
--- config/getHome.js
+++ config/getHome.js
@@ -9,11 +9,7 @@ module.exports = {
 			
 			let totalAmount = 0
 			let itemsPerValue = 0
-			// let valueOfHomeProperty = 0
-			// let valueOfTraffic = 0
-			// let valueOfEntertainment = 0
-			// let valueOfFood = 0
-			// let valueOfOthers = 0
+			
 			//let filteredMonth = new Date().getMonth() + 1
 			let filteredMonth = 8
 			const recordsDisplay = await Record.find({
@@ -21,112 +17,23 @@ module.exports = {
 					$gte: `2019-0${filteredMonth}-01T12:11:28.774+00:00`,
 					$lt: `2019-0${filteredMonth+1}-01T12:11:28.774+00:00`}})
 			recordsDisplay.forEach((item) =>
-				//console.log('item.amount', item.amount)
 				totalAmount += parseInt(item.amount)  
 			)
-			console.log('totalAmount', totalAmount)
-		  	// for (let i = 0; i < recordsDisplay.length; i++) {
-				// 	totalAmount += parseInt(recordsDisplay[i].amount)
-				// }
-			//display default /home page records of month
-			// Record.find({ userID: req.user._id }, (err, records) => {
-			// 	if (err) return console.error(err)
-			// 	const recordsDisplay = records.filter(({ date }) => {
-			// 		if (date.getMonth() + 1 === filteredMonth) {
-			// 			return date.getMonth(filteredMonth)
-			// 		}
-			// 	})
-			//console.log('recordsDisplay', recordsDisplay)
+			
 			let chartData = []
 			let categoryItemArray = ['homeProperty', 'traffic', 'entertainment','food','others']
-			//let categoryItemArray = ['homeProperty']
 
 			categoryItemArray.forEach(function (items) {
 				const chartDataPerItem = recordsDisplay.filter(({ category }) => {
 					return category.includes(items)
 				})
-				console.log('chartDataPerItem', chartDataPerItem)
-				// for (let j = 0; j < chartDataPerItem.length; j++) {
-				// 	itemsPerValue += (chartDataPerItem[j].amount) / totalAmount * 100
-				// 	itemsPerValue = Math.round(itemsPerValue)
-				// }
-
-				///////// check itemsPerValue is wrong//////
 				chartDataPerItem.forEach((item) =>{
 					itemsPerValue += parseInt(item.amount) 
-					console.log('itemsPerValue', itemsPerValue)
 					itemsPerValue = Math.round(itemsPerValue/totalAmount*100)
-					console.log('itemsPerValue', itemsPerValue)
 				})
 				chartData.push(itemsPerValue)
-				console.log('chartData', chartData)
-			});
-			// const chartDataPerItem = recordsDisplay.filter(({ category }) => {
-
-			// 	return category.includes('homeProperty')
-
-			// })
-
-			// const chartDataHomeProperty = recordsDisplay.filter(({ category }) => {
-
-			// 	return category.includes('homeProperty')
-
-			// })
-
-			// const chartDataTraffic = recordsDisplay.filter(({ date, category }) => {
-
-			// 	return category.includes('traffic')
-
-			// })
-
-			// const chartDateEntertainment = recordsDisplay.filter(({ date, category }) => {
-
-			// 	return category.includes('entertainment')
-
-			// })
-
-			// const chartDataFood = recordsDisplay.filter(({ date, category }) => {
-
-			// 	return category.includes('food')
-
-			// })
-
-			// const chartDataOthers = recordsDisplay.filter(({ date, category }) => {
-
-			// 	return category.includes('others')
-
-			// })
-
-			// //if (err) return console.error(err)
-			// if (totalAmount == 0) {
-			// 	for (let i = 0; i < recordsDisplay.length; i++) {
-			// 		totalAmount += parseInt(recordsDisplay[i].amount)
-			// 	}
-			// 	for (let j = 0; j < chartDataHomeProperty.length; j++) {
-			// 		valueOfHomeProperty += (chartDataHomeProperty[j].amount) / totalAmount * 100
-			// 		valueOfHomeProperty = Math.round(valueOfHomeProperty) - 1
-			// 	}
-			// 	for (let k = 0; k < chartDataTraffic.length; k++) {
-			// 		valueOfTraffic += (chartDataTraffic[k].amount) / totalAmount * 100
-			// 		valueOfTraffic = Math.round(valueOfTraffic)
-			// 	}
-			// 	for (let i = 0; i < chartDateEntertainment.length; i++) {
-			// 		valueOfEntertainment += (chartDateEntertainment[i].amount) / totalAmount * 100
-			// 		valueOfEntertainment = Math.round(valueOfEntertainment)
-			// 	}
-			// 	for (let i = 0; i < chartDataFood.length; i++) {
-			// 		valueOfFood += (chartDataFood[i].amount) / totalAmount * 100
-			// 		valueOfFood = Math.round(valueOfFood)
-			// 	}
-			// 	for (let i = 0; i < chartDataOthers.length; i++) {
-			// 		valueOfOthers += (chartDataOthers[i].amount) / totalAmount * 100
-			// 		valueOfOthers = Math.round(valueOfOthers)
-			// 	}
-			// }
-
-			// let chartData = [valueOfHomeProperty, valueOfTraffic, valueOfEntertainment, valueOfFood, valueOfOthers]
+			})
 			console.log('chartData in gethome.js', chartData)
-
 			res.render('index', {
 				records: recordsDisplay,
 				totalAmount: totalAmount,
@@ -135,9 +42,6 @@ module.exports = {
 			})
 		} catch (err) {
 			return console.log(err)
-		}
-		
-		//return next()		
-   
+		}  
 	}
 }	
