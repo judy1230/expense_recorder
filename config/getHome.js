2aadc535dd98b450886fa03e3d5af8ea5a99ca62
JudyLin <www.jiuky@gmail.com>
2019-10-23 12:07:39 +0800

    modify gehome.js coding style

--- config/getHome.js
+++ config/getHome.js
@@ -6,8 +6,8 @@ const { getCategoryItem } = require('../config/getCategoryItem.js')
 const { Op } = require('sequelize')
 module.exports = {
 
-  getHome: async(req, res) => {		
-		try{			
+	getHome: async (req, res) => {
+		try {
 			let totalAmount = 0
 			let filteredMonth = new Date().getMonth() + 1
 			let chartData = []
@@ -15,9 +15,9 @@ module.exports = {
 			const records = await User.findByPk(req.user.id)
 				.then((user) => {
 					if (!user) throw new Error("user not found")
-					return Record.findAll({ 
+					return Record.findAll({
 						raw: true,
-						where: { 
+						where: {
 							UserId: req.user.id,
 							date: {
 								[Op.between]: [`2019-0${filteredMonth}-01`, `2019-0${filteredMonth}-30`]
@@ -34,8 +34,8 @@ module.exports = {
 						totalAmount: totalAmount,
 						filteredMonth: filteredMonth,
 						chartData: chartData
-					})	
-			  }).catch((error) => { return res.status(422).json(error)}) 		
-		  } catch(err) { return console.log(err)}
+					})
+				}).catch((error) => { return res.status(422).json(error) })
+		} catch (err) { return console.log(err) }
 	}
 }	
